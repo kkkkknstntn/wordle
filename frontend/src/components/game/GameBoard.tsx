9b91a2c6d4274f35e493a031052c73130931014d
Константин Михайлин <mik-dmitry.k@yandex.ru>
2024-11-10 19:13:15 +0400

    updated the keyboard: changed the layout to the standard QWERTY; made a new Enter button; fixed the display of used letters on the keyboard

--- frontend/src/components/game/GameBoard.tsx
+++ frontend/src/components/game/GameBoard.tsx
@@ -1,106 +1,105 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Player } from '../../types/player';
-import DefaultButton from '../defaultButton/DefaultButton';
 import PanelRow from './PanelRow';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { selectCurrentGameState, tryAgain, tryAgainWithoutAuth } from '../../features/gameSlice';
-import { NewAttempt } from '../../types/game';
 import { useSelector } from 'react-redux';
 import GameKeyboard from './Keyboard';
 import GameStatusModel from './GameStatusModel';
 
-
 function GameBoard() {
   const [word, setWord] = useState('');
-  const [currentAttempt, setCurrentAttempt] = useState(0)
-  const [isEblan, setIsEblan] = useState(false)
-  const dispatch = useAppDispatch()
+  const [currentAttempt, setCurrentAttempt] = useState(0);
+  const [isEblan, setIsEblan] = useState(false);
+  const dispatch = useAppDispatch();
 
-  const { game_id, guessed_word, current_try, game_status, letter_statuses, isCorrectWord, isGameWithoutAuth } = useSelector(selectCurrentGameState)
-  const [copyLetter_statuses, setCopyLetter_statuses] = useState(letter_statuses)
+  const { game_id, guessed_word, current_try, game_status, letter_statuses, isCorrectWord, isGameWithoutAuth } = useSelector(selectCurrentGameState);
+  const [copyLetter_statuses, setCopyLetter_statuses] = useState(letter_statuses);
   const isFirstRender = useRef(true);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
-      if ('key' in event && typeof event.key === 'string' && /^[a-zA-Z]+$/.test(event.key)) {
-        if (event.key.length == 1 || event.key == 'Backspace')
-          handleLetterClick(event.key)
+      // Проверяем только нажатия букв (A-Z) и специальные клавиши
+      if (/^[a-zA-Z]$/.test(event.key)) {
+        handleLetterClick(event.key); // Обрабатываем ввод буквы
+      } else if (event.key === 'Backspace') {
+        handleLetterClick('Backspace'); // Обрабатываем Backspace
+      } else if (event.key === 'Enter') {
+        event.preventDefault(); // Предотвращаем стандартное поведение
+        handleSubmit(); // Вызываем функцию отправки при нажатии Enter
       }
+      // Игнорируем все остальные клавиши
     };
+
     window.addEventListener('keydown', handleKeyDown);
 
-    // Очистка обработчика при размонтировании компонента
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [word]);
 
   useEffect(() => {
-    setCopyLetter_statuses(letter_statuses)
-    setCurrentAttempt(current_try)
+    setCopyLetter_statuses(letter_statuses);
+    setCurrentAttempt(current_try);
+  }, [current_try]);
 
-  }, [current_try])
-
-  // Функция обработки нажатия буквы
   const handleLetterClick = (letter: string): void => {
-    // Добавляем нажатую букву к текущему слову
-    if (letter == 'Backspace') {
-      if (word.length > 0)
-        setWord(prevWord => prevWord.substring(0, prevWord.length - 1))
-    }
-    else if(word.length < 5)
-      setWord(prevWord => prevWord + letter);
-  };
-  
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if(word.length !== 5)
-      setIsEblan(true);
-    else {
-      console.log(game_id)
-      console.log(current_try + " " + game_status + " " + letter_statuses)
-      console.log(word)
-      if(isGameWithoutAuth)
-        await dispatch(tryAgainWithoutAuth({
-          game_id: game_id,
-          guessed_word: word
-        }))
-      else {
-        await dispatch(tryAgain({
-          game_id: game_id,
-          guessed_word: word
-        }))
+    if (letter === 'Backspace') {
+      if (word.length > 0) {
+        setWord(prevWord => prevWord.substring(0, prevWord.length - 1));
       }
-      setIsEblan(false)
-      setWord('')
+    } else if (word.length < 5) {
+      setWord(prevWord => prevWord + letter);
     }
-  }
+  };
+
+  const handleSubmit = async () => {
+    if (word.length !== 5) {
+      setIsEblan(true);
+      return;
+    }
+
+    console.log(game_id);
+    console.log(current_try + " " + letter_statuses);
+    console.log(word);
+
+    if (isGameWithoutAuth) {
+      await dispatch(tryAgainWithoutAuth({
+        game_id: game_id,
+        guessed_word: word
+      }));
+    } else {
+      await dispatch(tryAgain({
+        game_id: game_id,
+        guessed_word: word
+      }));
+    }
+
+    setIsEblan(false);
+    setWord('');
+  };
+
   return (
     <div className='gameWrapper'>
-      {game_status == "WIN" || game_status == "LOSE" ? <GameStatusModel/> : <></>}
+      {game_status === "WIN" || game_status === "LOSE" ? <GameStatusModel /> : null}
 
       <div className='viewBoard'>
         {Array.from({ length: 6 }, (_, index) => (
-           <PanelRow 
+          <PanelRow 
             key={index} 
             word={word} 
             isLocked={index !== currentAttempt} 
             letter_statuses={index + 1 === currentAttempt ? [...copyLetter_statuses] : undefined}
-            />
-          ))
-        }
+          />
+        ))}
       </div>
-      <DefaultButton 
-        text={'Ввести'} 
-        action={handleSubmit} 
-        extraClass='gameButton submitButton'
-        isDisabled={currentAttempt >= 6}
-      />
-      {isCorrectWord ? <></> : <>слово некорректно еблан <br/></>}
-      {isEblan ? <> введи 5 букв еблан</> : <></>}
+
+      {isCorrectWord ? null : <div>Слово некорректно</div>}
+      {isEblan ? <div>Введите 5 букв</div> : null}
+
       <GameKeyboard 
         onLetterClick={handleLetterClick} 
         onBackspaceClick={() => handleLetterClick('Backspace')} 
+        onEnterClick={handleSubmit}
         letter_statuses={letter_statuses} 
         word={guessed_word}
         isFirstRender={isFirstRender.current}
--- frontend/src/components/game/Keyboard.tsx
+++ frontend/src/components/game/Keyboard.tsx
@@ -1,53 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface GameKeyboardProps {
   onLetterClick: (letter: string) => void;
   onBackspaceClick: () => void;
+  onEnterClick: () => void;
   letter_statuses?: string[];
   word?: string;
   isFirstRender: boolean
 }
 
-const GameKeyboard: React.FC<GameKeyboardProps> = ({ onLetterClick, onBackspaceClick, letter_statuses, word, isFirstRender }) => {
-  const englishLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const GameKeyboard: React.FC<GameKeyboardProps> = ({ onLetterClick, onBackspaceClick, onEnterClick, letter_statuses, word, isFirstRender }) => {
+   const keyboardLayout = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace']
+  ];
   console.log("рендер равно " + isFirstRender)
+
+  const [words, setWords] = useState<string[]>([])
+  const [letterStatuses, setLetterStatuses] = useState<string[][]>([])
+  useEffect(() => {
+    if (!word) return;
+    setWords([...words, word])
+    if (!letter_statuses) return;
+    setLetterStatuses([...letterStatuses, letter_statuses])
+  }, [word])
+
+  const handleEnter = async () => {
+    console.log(words);
+    console.log(letterStatuses);
+    onEnterClick();
+  };
+  useEffect(() => {
+    setLetterStatuses([])
+    setWords([])
+  }, [])
 
   return (
     <div className='gameKeyboardWrapper'>
       <div className='gameKeyboard'>
-        {Array.from(englishLetters).map((letter) => {
-        const statusIndex = word?.toUpperCase()?.indexOf(letter);
-        // Проверяем, что statusIndex является числом и существует letter_statuses
-        if (statusIndex !== undefined && typeof statusIndex === 'number' && letter_statuses) {
-            const statusClass = `${letter_statuses[statusIndex]}`;
-            return (
-              <button
-                key={letter}
-                onClick={() => onLetterClick(letter)}
-                className={`gameButton ${statusClass}`}
-              >
-                {letter}
-              </button>
-            );
-          } else {
-            // Если условия не выполняются, возвращаем кнопку без класса
-            return (
-              <button
-                key={letter}
-                onClick={() => onLetterClick(letter)}
-                className="gameButton"
-              >
-                {letter}
-              </button>
-            );
-          }
-        })}
-        <button
-          onClick={onBackspaceClick}
-          className="gameButton backspaceButton"
-        >
-          Backspace
-        </button>
+        {keyboardLayout.map((row, rowIndex) => (
+          <div key={rowIndex} className="keyboardRow">
+            {row.map((letter) => {
+              if (letter === "Backspace") {
+                return (
+                  <button
+                    key={letter}
+                    onClick={onBackspaceClick}
+                    className="gameButton backspaceButton"
+                  >
+                    Backspace
+                  </button>
+                );
+              } else if (letter === "Enter") {
+                return (
+                  <button
+                    key={letter}
+                    onClick={handleEnter}
+                    className="gameButton enterButton"
+                    
+                  >
+                   Enter
+                  </button>
+                );
+              } else {
+                let statusIndex = undefined
+                let statusClassNumber = 0
+                let statusClass = ''
+                for (let i = words.length - 1; i >= 0; --i) {
+                  statusIndex = words[i].indexOf(letter)
+                  if (statusIndex !== undefined) {
+                    if (letterStatuses[i][statusIndex] === 'NOT_PRESENT')
+                      statusClassNumber = Math.max(statusClassNumber, 1)
+                    else if (letterStatuses[i][statusIndex] === 'MISPLACED')
+                      statusClassNumber = Math.max(statusClassNumber, 2)
+                    else if (letterStatuses[i][statusIndex] === 'CORRECT')
+                      statusClassNumber = Math.max(statusClassNumber, 3)
+                  }
+                }
+                if (statusClassNumber === 1)
+                  statusClass = 'NOT_PRESENT'
+                else if (statusClassNumber === 2)
+                  statusClass = 'MISPLACED'
+                else if (statusClassNumber === 3)
+                  statusClass = 'CORRECT'
+                return (
+                  <button
+                    key={letter}
+                    onClick={() => onLetterClick(letter)}
+                    className={`gameButton ${statusClass}`}
+                  >
+                    {letter}
+                  </button>
+                );
+              }
+            })}
+          </div>
+        ))}
       </div>
     </div>
   );
