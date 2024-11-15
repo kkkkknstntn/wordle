29709373497fd6c72748026048a818f776fe84a3
Krab1o <bogdansils@gmail.com>
2024-11-15 20:20:50 +0400

    baseURL change

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -3,10 +3,10 @@ import axios from 'axios'
 import { store } from '../features/store';
 
 export const axiosPublic = axios.create({ 
-  baseURL: 'http://127.0.0.1:80'
+  baseURL: 'http://v183683.hosted-by-vdsina.com:80'
 })
-export const axiosPrivate = axios.create({ baseURL: 'http://127.0.0.1:80' })
+export const axiosPrivate = axios.create({ baseURL: 'http://v183683.hosted-by-vdsina.com:80' })
 export const axiosRefresh = axios.create({ 
-  baseURL: 'http://127.0.0.1:80',
+  baseURL: 'http://v183683.hosted-by-vdsina.com:80',
   //withCredentials: true,
 })
